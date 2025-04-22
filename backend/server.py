7d7de7b2e152e1c62612891b8920d7acf43c51bc
purio <wizard.bass5@gmail.com>
2025-04-22 16:40:22 +0900

    サーバーのメイン処理にUDPハンドラーとクライアントクリーンアップスレッドを追加し、クライアント情報の最終更新日時を管理する機能を実装。メッセージ送信時の認証処理を実装。

--- backend/server.py
+++ backend/server.py
@@ -16,24 +16,15 @@ lock = threading.Lock()
 
 def main():
     tcp_thread = threading.Thread(target=tcp_handler, daemon=True)
+    udp_thread = threading.Thread(target=udp_handler, daemon=True)
+    cleanup_thread = threading.Thread(target=cleanup_clients, daemon=True, args=(rooms, timeout))
     tcp_thread.start()
+    udp_thread.start()
+    cleanup_thread.start()
+    # tcp_threadが終了するまで待機
     tcp_thread.join()
-    # udp_handler()
-    
-
-# 各クライアントの最終更新日時を取得して、一定時間送信していない場合は管理用の連想配列から削除
-# def cleanup_clients(clients, timeout=60):
-#     while True:
-#         now = datetime.datetime.now()
-#         remove_list = []
-#         with lock:
-#             for address, information in list(clients.items()):
-#                 if (now - information["last_seen"]).total_seconds() > timeout:
-#                     remove_list.append(address)
-#             for addr in remove_list:
-#                 print(f'Removing {addr} due to inactivity')
-#                 del clients[addr]
-#         time.sleep(10)
+    udp_thread.join()
+    cleanup_thread.join()
 
 def udp_handler():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -52,6 +43,8 @@ def udp_handler():
             roomname = body[:roomname_length].decode('utf-8')
             token = body[roomname_length:token_length].decode('utf-8')
             message = body[token_length:].decode('utf-8')
+            if authentication_token(roomname, address, token):
+                send_message(roomname, message, sock)
         except Exception as e:
             print(f'UDP error: {str(e)}')
 
@@ -100,16 +93,18 @@ def create_room(connection, address, username, roomname):
     try:
         client_token = uuid.uuid4()
         # 部屋が存在しない場合、部屋を新規に作成。
-        if roomname not in rooms:
-            rooms[roomname] = {
-                "host": {},
-                "guest": {}
-            }
+        with lock:
+            if roomname not in rooms:
+                rooms[roomname] = {
+                    "host": {},
+                    "guest": {}
+                }
         # 作成した部屋にホストとして追加。
-        rooms[roomname]["host"][address] = {
-            "user_name": username,
-            "client_token": client_token
-        }
+            rooms[roomname]["host"][address] = {
+                "user_name": username,
+                "client_token": client_token,
+                "last_seen": datetime.datetime.now()
+            }
         print(f'client_token: {client_token}')
         send_token(connection, client_token, 1)
     except Exception as e:
@@ -119,10 +114,12 @@ def join_room(connection, address, username, roomname):
     try:
         client_token = uuid.uuid4()
         # ゲスト（ホスト以外のユーザー）として部屋に追加
-        rooms[roomname]["guest"][address] ={
-            "user_name": username,
-            "client_token": client_token,
-        }
+        with lock:
+            rooms[roomname]["guest"][address] ={
+                "user_name": username,
+                "client_token": client_token,
+                "last_seen": datetime.datetime.now()
+            }
         send_token(connection, client_token, 2)
     except Exception as e:
         print(f'Error joinging room: {e.__traceback__}')
@@ -137,6 +134,59 @@ def send_token(connection, token_row, status_code):
         connection.send(data)
     except Exception as e:
         print(f'Error sending token: {e.__class__.__name__}: {str(e)}')
+
+# udp接続時の認証
+def authentication_token(roomname, address, token):
+    if roomname not in rooms:
+        return False
+    if address not in rooms[roomname]["host"] or address not in rooms[roomname]["guest"]:
+        return False
+    #　クライアントから送信されたトークンが一致した場合TRUEを返す。
+    if address in rooms[roomname]["guest"]:
+        if rooms[roomname]["guest"][address]["client_token"] == token:
+            return True
+        else:
+            return False
+
+# その部屋に属するユーザーにメッセージを送信する処理
+def send_message(roomname, message, sock):
+    try:
+        # 部屋が存在しない場合はエラー
+        if roomname not in rooms:
+            raise Exception(f'room {roomname} is not found')
+        
+        # その部屋のホストにメッセージを送信
+        for address in rooms[roomname]["host"]:
+            try:
+                sock.sendto(message, address)
+            except Exception as e:
+                print(f'Error sending message to {address}')
+        # その部屋のゲストにメッセージを送信
+        for address in rooms[roomname]["guest"]:
+            try:
+                sock.sendto(message, address)
+            except Exception as e:
+                print(f'Error sending message to {address}')
+    except Exception as e:
+        print(f'An error occurred sending message: {str(e)}')
+
+# 部屋に属するユーザーの最終更新日時を取得して、一定時間動きがない場合は部屋から削除
+def cleanup_clients(rooms, timeout=60):
+    while True:
+        now = datetime.datetime.now()
+        remove_host = []
+        remove_guest = []
+        with lock:
+            for roomname, information in list(rooms.items()):
+                for address, user in list(information["host"].items()):
+                    if (now - user["last_seen"]).total_seconds() > timeout:
+                        del information["host"][address]
+                        # ホストが削除された場合、部屋ごと削除？
+                        # del rooms[roomname]
+                for address, user in list(information["guest"].items()):
+                    if (now - user["last_seen"]).total_seconds() > timeout:
+                        del information["guest"][address]            
+        time.sleep(30)
 
 if __name__ == "__main__":
     main()
